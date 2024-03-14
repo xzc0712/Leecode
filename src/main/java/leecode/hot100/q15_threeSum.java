8f5bca3cc2f560e94e265608aa896828c34aeb73
xzc <874448837@qq.com>
2024-03-14 10:50:38 +0800

    start q15_threeSum

--- src/main/java/leecode/hot100/q15_threeSum.java
+++ src/main/java/leecode/hot100/q15_threeSum.java
@@ -4,6 +4,11 @@ package leecode.hot100;
 import java.util.List;
 
 public class q15_threeSum {
+
+    //想法：先固定好最小值作为定k值(初始为0)，left=k+1,right = nums.length -1
+    // 如果left.v>0 退出循环
+    // 如果三者之和大于0，则需要缩小，right--
+    // 如果三者之和小于0，则需要增加，left++
     public List<List<Integer>> threeSum(int[] nums) {
         return null;
     }
